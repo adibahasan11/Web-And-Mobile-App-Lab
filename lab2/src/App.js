fd7e101c2bdf47a2f2e12280f22eeee6ffb35b00
Adiba Hasan <56604392+adibahasan11@users.noreply.github.com>
2022-01-05 21:56:35 +0600

    Revert "Revert "Adding Timer""
    This reverts commit 7a1e6688e928d448802740b3765bfeac2b8c89cf.

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { TimerProvider } from './Components/TimerContext';
+import AddTimer from './Components/AddTimer';
+import TimerList from './Components/TimerList';
 
 function App() {
   return (
+    <TimerProvider>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddTimer />
+        <TimerList />
       </header>
     </div>
+    </TimerProvider>
   );
 }
 
